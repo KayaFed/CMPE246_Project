ae588e1e0420a7ea68ca9dd3eeb16f57fdcb394c
KayaFed <kmfedechko@gmail.com>
2026-03-24 21:32:36 -0700

    Added hardware mode flag to config

--- src/config.py
+++ src/config.py
@@ -1,6 +1,5 @@
 # src/config.py For testing withought lights 
-
-
-#Once the pi is hooked up, update to correct wiring
+# Toggle between hardware LEDs and simulated LEDs
+USE_HARDWARE = False
 
 LED_PINS = list(range(64))  # simulate full chess board
